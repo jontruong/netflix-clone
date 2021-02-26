aeaa4683ec662144291ef5a8c6b166f970a896bb
Jonathan Truong <jjtruong90@gmail.com>
2021-02-25 19:56:48 -0800

    Adds trailers

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,4 @@
 const API_KEY = "89b300ea80fb48544d389b9905ab1ad8";
-
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
